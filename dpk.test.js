6f30f48e604dde990b67c2f8d659145e3cd3e72d
Abdulsamii Ajala <piftech@pif-app.com>
2023-04-09 18:50:52 +0100

    test: deterministicPartitionKey unit tests

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,32 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it('returns partitionKey from event when provided', () => {
+    const partitionKey = 'testKey';
+    const event = { partitionKey };
+    expect(deterministicPartitionKey(event)).toBe(partitionKey);
+  })
+
+  it("returns a hash based on event data when partitionKey is not provided", () => {
+    const event = { key: "value" };
+    const hash = deterministicPartitionKey(event);
+    expect(hash).not.toBe(undefined);
+    expect(hash).toHaveLength(128); // sha3-512 hash length
+  });
+
+  it("returns JSON string when partitionKey is a non-string object", () => {
+    const partitionKey = { key: "value" };
+    const event = { partitionKey };
+    const jsonString = deterministicPartitionKey(event);
+    expect(jsonString).toBe(JSON.stringify(partitionKey));
+  });
+
+  it("returns a hash when partitionKey length is greater than MAX_PARTITION_KEY_LENGTH", () => {
+    const partitionKey = "a".repeat(257);
+    const event = { partitionKey };
+    const hash = deterministicPartitionKey(event);
+    expect(hash).not.toBe(undefined);
+    expect(hash).toHaveLength(128);
+  });
 });
